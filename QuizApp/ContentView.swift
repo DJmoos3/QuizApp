aae88f0e0847b913494029025662610cbe313261
DJmoos3 <isaac.strandh@gmail.com>
2026-03-30 22:55:13 +0200

    Added a very basic version of the point system to work on later

--- QuizApp/ContentView.swift
+++ QuizApp/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentScore: Int = 0
+    @State private var highScore: Int = 0
+    @State private var isCorrect = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Button("Example button"){
+                if isCorrect{
+                    currentScore += 1
+                }else{
+                    highScore = currentScore
+                }
+            }.buttonStyle(.borderedProminent)
         }
         .padding()
     }
